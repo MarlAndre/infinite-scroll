07fbde370ce0df076f8d8f669254958f5c8e0ccb
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-20 09:55:53 -0400

    adding infinite scroll

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = "BQC_jBlRVPSMGZ85M-b9s62y3MEAjo3iKsXcUgMv2As";
+const apiKey = "7GQpmQ15VIqOv9gw5zTP3DZlsNtAIOkTqGsanlTFXSQ;";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//  Check if all image were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper function to set attributes on DOM elements
 function setAttributes(element, attributes) {
@@ -17,6 +29,8 @@ function setAttributes(element, attributes) {
 
 // Create elements for links & Photos and add them to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // For each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -25,13 +39,15 @@ function displayPhotos() {
       href: photo.links.html,
       target: "_blank",
     });
-
+    // Create <img>
     const img = document.createElement("img");
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
     // Put <img> inside <a>, and both of them inside imageContainer element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -48,6 +64,17 @@ async function getPhotos() {
     // Catch error here
   }
 }
+
+// Add scroll event listener
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On load
 getPhotos();

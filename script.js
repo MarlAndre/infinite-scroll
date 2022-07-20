78eb7ffeb8a79d90d4b6817e833047e5e8db619e
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-20 10:00:32 -0400

    improving performance

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 5;
 const apiKey = "7GQpmQ15VIqOv9gw5zTP3DZlsNtAIOkTqGsanlTFXSQ;";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -17,6 +17,7 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    count = 30;
   }
 }
 
